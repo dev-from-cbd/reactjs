6237b7c72df019611151f07dbf3cd09498a8284c
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-15 09:01:21 +1100

    react

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { EventEmitter } from "fbemitter";
+
+export const emitter = new EventEmitter();
 
 const Notification = () => {
-  return <div>Notification</div>;
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState();
+  const notificationStyle = {
+    padding: 10,
+    border: "1px green solid",
+    marginBottom: 15,
+    backgroundColor: "#55c555",
+    borderRadius: 2,
+  };
+  const reset = () => {
+    setOpen(false);
+  };
+  const autoHideAfterTimeout = () => {
+    setTimeout(() => reset(), 5000);
+  };
+  emitter.addListener("NOTIFICATION", (msg) => {
+    setMessage(msg);
+    setOpen(true);
+    autoHideAfterTimeout();
+  });
+  if (!open) {
+    return null;
+  }
+  return (
+    <div style={notificationStyle}>
+      <span>{message}</span>
+    </div>
+  );
 };
 
 export default Notification;
--- src/containers/todoContainer/index.js
+++ src/containers/todoContainer/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Todo from "../../components/todoList";
 import AddTodo from "../../components/addTodo";
-
+import Notification from "../../components/notification";
 const todoList = [
-  { id: 1, title: "title #1", done: true },
-  { id: 2, title: "Create a task", done: false },
+  { id: 1, title: "Learn React", done: true },
+  { id: 2, title: "Create a todo Application", done: false },
 ];
 const TodoContainer = () => {
   const [todos, setTodos] = useState(todoList);
@@ -12,33 +12,32 @@ const TodoContainer = () => {
     const newTodoList = [...todos, newTodo];
     setTodos(newTodoList);
   };
-
   const handleRemoveTodo = (id) => {
     const newTodoList = todos.filter((todo) => todo.id !== id);
     setTodos(newTodoList);
-    //console.log(id);
   };
-
-  const handleCheckboxChange = (id) => {
+  const hanldeCheckboxChange = (id) => {
     const newTodoList = todos.map((todo) => {
       if (todo.id === id) return { ...todo, done: !todo.done };
+      return todo;
     });
-    setTodos(newTodoList)
+    setTodos(newTodoList);
   };
-
   return (
     <div style={{ margin: 20 }}>
-      <h4 align="center">React JS Web App</h4>
+      <h4 align="center">Todo Application</h4>
+      <Notification />
       {todos.length > 0 ? (
         todos.map((todo) => (
           <Todo
             todo={todo}
+            key={todo.id}
             removeTodo={handleRemoveTodo}
-            handleChange={handleCheckboxChange}
+            handleChange={hanldeCheckboxChange}
           />
         ))
       ) : (
-        <p>No todo tasks now</p>
+        <p align="center">no todo left</p>
       )}
       <AddTodo addTodo={handleAddTodo} />
     </div>
